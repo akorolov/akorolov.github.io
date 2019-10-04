2ea5d767ed84635c98b5859813fca6c609c70bc3
Anastasia <anastasia@korolov.com>
2019-10-04 17:02:07 -0400

    fixed a bad problem

--- tracks.js
+++ tracks.js
@@ -19,11 +19,13 @@ const SortableList = {
   `
 };
 
+var newname = "";
+
 const SortableItem = {
   mixins: [ElementMixin],
   props: ['item'],
   template: `
-    <li class="list-item"><button class="secret" v-on:click="activateInEditMode" v-show="!isEditing">{{item}}</button><span v-show="isEditing" ><input v-model="newerName" type="text" class="text-input2" v-on:keyup.enter="updateCharacter"></span>  <button class="delete" v-on:click="remove">X</button></li>
+    <li class="list-item"><button class="secret" v-on:click="activateInEditMode" v-show="!isEditing">{{item}}</button><span v-show="isEditing" ><input v-model="newerName" type="text" class="text-input2" v-on:keyup.enter="updates"></span>  <button class="delete" v-on:click="remove">X</button></li>
   `,
   data() {
   return {
@@ -38,9 +40,11 @@ const SortableItem = {
     activateInEditMode: function() {
         this.isEditing = true
     },
-    updateCharacter: function() {
+    updates: function() {
       this.isEditing = false;
       this.item = this.newerName;
+      newname = this.newerName;
+      this.$emit('updated');
     }
   }
 };
@@ -59,11 +63,18 @@ const ExampleVue = {
       var index = this.items.indexOf(item);
       this.items.splice(index, 1);
     },
+    updateCharacter: function(item) {
+      console.log(item);
+      var index = this.items.indexOf(item);
+      console.log(index);
+      this.items[index] = newname;
+      console.log(this.items);
+    },
   },
   template: `
     <div class="root">
       <SortableList lockAxis="y" v-model="items">
-        <SortableItem v-for="(item, index) in items" :index="index" :key="index" :item="item" @removed='removeCharacter(item)'/>
+        <SortableItem v-for="(item, index) in items" :index="index" :key="index" :item="item" @removed='removeCharacter(item)' @updated='updateCharacter(item)'/>
       </SortableList><br>
       <input type="text" placeholder="Add a character" autofocus class="text-input" v-model="newName" v-on:keyup.enter="addCharacter">
     </div>
